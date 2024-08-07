f02c6c5d92224436a5d07ef93d5ae77413e85399
AshwinShivaa <ashwinshivaaduraisamy@gmail.com>
2024-08-07 08:03:44 +0530

    first commit

---
+++ src/Footer.jsx
@@ -0,0 +1,15 @@
+
+import React from "react";
+
+
+function Footer() {
+  return (
+    <div className="footer-main">
+      <footer>
+        <p>&copy; 2024 Price Card</p>
+      </footer>
+    </div>
+  );
+}
+
+export default Footer;
